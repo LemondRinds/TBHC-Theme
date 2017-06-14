b1e84ba6bf306cf84f4dfb5d4f1da595fdfcf04a
LemondRinds <diveaway89@gmail.com>
2017-06-14 12:51:07 -0400

    more search box

--- functions.php
+++ functions.php
@@ -836,7 +836,7 @@ function frontpage_scholarship_spotlight() {
 						<label for="search">Search in <?php echo home_url( '/' ); ?></label>
 						<input type="text" name="s" id="search" value="<?php the_search_query(); ?>" />
 						<input type="image" alt="Search" src="<?php bloginfo( 'template_url' ); ?>/images/search.png" />
-						<input type="hidden" value="post" name="spotlight" id="spotlight" />
+						<input type="hidden" value="spotlight" name="post_type" id="post_type" />
 					</form>
 				</div>
 			</div>
