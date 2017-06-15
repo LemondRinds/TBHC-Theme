845a17d08f8f7cb97bedf872966b776dafce96bc
LemondRinds <diveaway89@gmail.com>
2017-06-15 11:51:33 -0400

    debugging search junk

--- search.php
+++ search.php
@@ -1,14 +1,5 @@
-<?php $options = get_option(THEME_OPTIONS_NAME);?>
 <?php get_header(); ?>
-<?php if ($options['enable_google'] or $options['enable_google'] === null){ ?>
+
 Technically working...
-<? }else{ ?>
-<?php
-	$domain  = $options['search_domain'];
-	$limit   = (int)$options['search_per_page'];
-	$start   = (is_numeric($_GET['start'])) ? (int)$_GET['start'] : 0;
-	$results = get_search_results($_GET['s'], $start, $limit, $domain);
-?>
-I'm just some stuff on a page!
-<? } ?>
+
 <?php get_footer();?>
